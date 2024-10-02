2f8346d1fad0f481ab5e4be3a376078ce863f783
yena040717 <163421642+yena040717@users.noreply.github.com>
2024-10-02 23:06:13 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+    int n = 5;
+
+    for (i = 1; i <= n; i++)
+    {
+        
+        for (j = i; j < n; j++)
+        {
+            printf(" ");
+        }
+        
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
