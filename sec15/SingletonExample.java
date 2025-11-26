56a27c3e0603ea9d77cd0ad7bcefed9d8cde00a8
yena040717 <163421642+yena040717@users.noreply.github.com>
2025-11-26 21:25:17 +0900

    Add files via upload

---
+++ sec15/SingletonExample.java
@@ -0,0 +1,21 @@
+package ch06.sec15;
+
+public class SingletonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Singleton obj1=new Singleton();
+		//Singleton obj2=new Singleton();
+		
+		Singleton obj1=Singleton.getInstance();
+		Singleton obj2=Singleton.getInstance();
+		
+		if(obj1==obj2) {
+			System.out.println("같은 Singleton 객체입니다.");
+		}else {
+			System.out.println("다른 Singleton 객체입니다.");
+		}
+
+	}
+
+}
